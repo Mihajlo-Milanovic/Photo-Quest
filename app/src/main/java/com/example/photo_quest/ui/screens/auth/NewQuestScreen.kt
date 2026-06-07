5c6b258d6ea10be87b2eb5f448096db90f34d7b5
Mihajlo <milanovic.mihajlo.8@gmail.com>
2026-06-07 22:23:15 +0200

    feat(new-quest): Added new quest screen

---
+++ app/src/main/java/com/example/photo_quest/ui/screens/auth/NewQuestScreen.kt
@@ -0,0 +1,26 @@
+package com.example.photo_quest.ui.screens.auth
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NewQuestScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Add quest",
+        )
+    }
+}
