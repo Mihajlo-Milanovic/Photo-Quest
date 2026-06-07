0b070b6b2876239b950169d5ae8423355a451430
Mihajlo <milanovic.mihajlo.8@gmail.com>
2026-06-07 23:48:15 +0200

    feat(search): Added search screen

---
+++ app/src/main/java/com/example/photo_quest/ui/screens/SearchScreen.kt
@@ -0,0 +1,26 @@
+package com.example.photo_quest.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Search",
+        )
+    }
+}
